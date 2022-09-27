5b7d0d4eb3acf2557296bd6d12af9dde4bb66388
Geethika-30 <112071177+Geethika-30@users.noreply.github.com>
2022-09-27 20:07:49 +0530

    Add files via upload

---
+++ stack applications.c
@@ -0,0 +1,35 @@
+#include <stdio.h>  
+#include <string.h>  
+  
+#define max 100  
+int top,stack[max];  
+  
+void push(char x){  
+  
+       
+      if(top == max-1){  
+          printf("stack overflow");  
+      }  else {  
+          stack[++top]=x;  
+      }  
+  
+}  
+  
+void pop(){  
+    
+      printf("%c",stack[top--]);  
+}  
+  
+  
+main()  
+{  
+   char str[]="sri lanka";  
+   int len = strlen(str);  
+   int i;  
+  
+   for(i=0;i<len;i++)  
+        push(str[i]);  
+  
+   for(i=0;i<len;i++)  
+      pop();  
+} 
